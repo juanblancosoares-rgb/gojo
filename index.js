c379f4e2cfec6c597f5d2a4e79a5e83c37e7f4ca
juanblancosoares-rgb <juan.blanco.soares@escola.pr.gov.br>
2026-03-26 11:52:01 -0300

    gojo

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ let n1 = 56;
 let n2 = 98;
 const soma = n1 + n2;
 const sub = n1 - n2;
-let mensagem = 'O resultado da soma é {soma}, e o resultado da subtração é {sub}'.replace('{soma}', soma) .replace('{sub}', sub);
+const divisão = n1/n2;
+const multiplicação = n1 * n2;
+let mensagem = 'O resultado da mutiplicação é {multiplicação},O resultado da soma é {soma}, e o resultado da subtração é {sub}'.replace('{soma}', multiplicação , soma) .replace('{sub}', multiplicação, sub);
 let msg2 = soma >=100? 'Sua soma é maior que 100': 'Sua soma é menor que 100';
 let msg = sub >=100? 'Sua subtração é maior que 100': 'Sua subtração é menor que 100';
+let msg3 = multiplicação >=100? 'sua multiplicação é maior que 100': 'Sua multiplicação é menor que 100';
+
 console.log (mensagem);
 console.log (msg2);
 console.log (msg);
+console.log (msg3);
